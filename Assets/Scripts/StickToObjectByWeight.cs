961f0b3049bc087f1e576e20086692f9864b5d42
Daimnon <49341329+Daimnon@users.noreply.github.com>
2023-01-03 20:13:56 +0200

    More progress with pompa

--- Assets/Scripts/StickToObjectByWeight.cs
+++ Assets/Scripts/StickToObjectByWeight.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class StickToObjectByWeight : MonoBehaviour
 {
-    [SerializeField] private float _maxPullMass = 25;
+    [SerializeField] private float _maxPullMass = 25, lerpDuration = 3;
+
+    private const string _weaponTag = "Rifle";
+    private Rigidbody _objectRb;
+    private Vector3 _targetPos = Vector3.zero;
+
+    private void Start()
+    {
+        _targetPos = transform.position;
+    }
+    private void Update()
+    {
+        if (_objectRb && !_objectRb.gameObject.CompareTag(_weaponTag))
+            transform.position = _objectRb.transform.position;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.TryGetComponent(out Rigidbody rb) && rb.mass <= _maxPullMass)
+        if (!other.gameObject.CompareTag(_weaponTag) && other.gameObject.TryGetComponent(out Rigidbody rb) && rb.mass <= _maxPullMass)
         {
-            transform.position = other.transform.position;
+            StartCoroutine(PullObject(_targetPos, lerpDuration));
+            _objectRb = rb;
         }
+    }
+
+    IEnumerator PullObject(Vector3 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+        while (time < duration)
+        {
+            _objectRb.transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
     }
 }
