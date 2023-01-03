19ae22d4a46fd0c2d2da7fe102e27cf175bbee61
Daimnon <49341329+Daimnon@users.noreply.github.com>
2023-01-03 15:49:34 +0200

    Update RifleHandler.cs

--- Assets/Scripts/RifleHandler.cs
+++ Assets/Scripts/RifleHandler.cs
@@ -8,7 +8,7 @@ public enum RifleType { Regular, Super, Pompa }
 public class RifleHandler : MonoBehaviour
 {
     [SerializeField] GameObject _bulletPrefab, _bulletContainer;
-    [SerializeField] Transform _bulletSpawnPoint, _grappleOrigin;
+    [SerializeField] Transform _bulletOriginTr;
     [SerializeField] RifleType _rifleType;
     [SerializeField] Material _grappleMat;
     [SerializeField] float _grappleStartWidth, _grappleEndWidth;
@@ -17,56 +17,102 @@ public class RifleHandler : MonoBehaviour
     public float BulletSpeedMultiplier;
     
     private LineRenderer _lineRenderer;
-    private bool _isEquiped = false, _canFire = false;
-    
+    private bool _isEquiped = false, _canFire = false, _pompaLive = false;
+    private Vector3 _lastAnchoredPos = Vector3.up, _lastAnchoredDir = Vector3.zero;
+
+    #region MonoBehaviour Callbacks
     private void Update()
     {
         Debug.Log($"{LeftpinchAnimationAction.action.ReadValue<float>()}");
         Debug.Log($"{RightpinchAnimationAction.action.ReadValue<float>()}");
 
-        if (RightpinchAnimationAction.action.triggered)
+        if (_isEquiped && _canFire)
         {
-            switch (_rifleType)
-            {
-                case RifleType.Regular:
-                    Fire();
-                    break;
-                //case RifleType.Super:
-                //    SuperFire();
-                //    break;
-                case RifleType.Pompa:
-                    PompaFire();
-                    break;
-                default:
-                    break;
-            }
-        }
-        if (LeftpinchAnimationAction.action.IsPressed() || RightpinchAnimationAction.action.IsPressed())
-        {
-            if (_isEquiped && _canFire)
-            {
-                switch (_rifleType)
-                {
-                    //case RifleType.Regular:
-                    //    Fire();
-                    //    break;
-                    case RifleType.Super:
-                        SuperFire();
-                        break;
-                    //case RifleType.Pompa:
-                    //    PompaFire();
-                    //    break;
-                    default:
-                        break;
-                }
-            }
+            WeaponFireBtnDown();
+            WeaponFireBtn();
+            WeaponFireUp();
         }
     }
+    #endregion
 
+    #region Fire Handling
+    private void WeaponFireBtnDown()
+    {
+        if (RightpinchAnimationAction.action.triggered)
+        {
+            ChooseRifleOnFireTrigger();
+        }
+    }
+    private void WeaponFireBtn()
+    {
+        if (LeftpinchAnimationAction.action.IsPressed() || RightpinchAnimationAction.action.IsPressed())
+        {
+            ChooseRifleOnFireContinue();
+        }
+    }
+    private void WeaponFireUp()
+    {
+        if (RightpinchAnimationAction.action.WasReleasedThisFrame())
+        {
+            ChooseRifleOnRelease();
+        }
+    }
+    #endregion
+
+    #region Rifle Choosing
+    private void ChooseRifleOnFireTrigger()
+    {
+        switch (_rifleType)
+        {
+            case RifleType.Regular:
+                Fire();
+                break;
+            case RifleType.Super:
+                return;
+            case RifleType.Pompa:
+                PompaFire();
+                break;
+            default:
+                break;
+        }
+    }
+    private void ChooseRifleOnFireContinue()
+    {
+        switch (_rifleType)
+        {
+            case RifleType.Regular:
+                return;
+            case RifleType.Super:
+                SuperFire();
+                break;
+            case RifleType.Pompa:
+                AdjustLineRendererToBullet();
+                break;
+            default:
+                break;
+        }
+    }
+    private void ChooseRifleOnRelease()
+    {
+        switch (_rifleType)
+        {
+            case RifleType.Regular:
+                return;
+            case RifleType.Super:
+                return;
+            case RifleType.Pompa:
+                ReleasePompa();
+                break;
+            default:
+                break;
+        }
+    }
+    #endregion
+
+    #region Pompa Behavior
     private void PompaFire()
     {
-
-        GameObject projectile = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity, _bulletContainer.transform);
+        GameObject projectile = Instantiate(_bulletPrefab, _bulletOriginTr.position, Quaternion.identity, _bulletContainer.transform);
         projectile.GetComponent<Rigidbody>().AddForce(this.transform.up.normalized * BulletSpeedMultiplier, ForceMode.Impulse);
 
         // Initialize Line -------------------------------
@@ -79,24 +125,56 @@ public class RifleHandler : MonoBehaviour
 
         // Set Line -------------------------------
         Vector3[] lrPositions = new Vector3[2];
-        lrPositions[0] = _grappleOrigin.position;
+        lrPositions[0] = _bulletOriginTr.position;
         lrPositions[1] = projectile.transform.position;
         _lineRenderer.SetPositions(lrPositions);
+        _bulletOriginTr = projectile.transform;
         // ----------------------------------------
 
-
-
+        _pompaLive = true;
     }
+    private void AdjustLineRendererToBullet()
+    {
+        if (_pompaLive)
+        {
+            if (_lineRenderer)
+            {
+                _lineRenderer.SetPosition(0, _bulletOriginTr.position);
+            }
+            else
+            {
+                Debug.Log("No LineRenderer");
+                return;
+            }
+        }
+    }
+    private void ReleasePompa()
+    {
+        _lastAnchoredPos = Vector3.up;
+        _lastAnchoredDir = Vector3.zero;
+    
+        if (_lineRenderer)
+            Destroy(_lineRenderer.gameObject);
+    }
+    #endregion
+
+    #region SuperRifle Behavior
     private void SuperFire()
     {
-        var firedBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity, _bulletContainer.transform);
+        var firedBullet = Instantiate(_bulletPrefab, _bulletOriginTr.position, Quaternion.identity, _bulletContainer.transform);
         firedBullet.GetComponent<Rigidbody>().AddForce(this.transform.up.normalized*BulletSpeedMultiplier, ForceMode.Impulse);
 
     }
+    #endregion
+
+    #region Fire Behavior
     private void Fire()
     {
-        GameObject firedBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity, _bulletContainer.transform);
+        GameObject firedBullet = Instantiate(_bulletPrefab, _bulletOriginTr.position, Quaternion.identity, _bulletContainer.transform);
     }
+    #endregion
+
+    #region OnClick
     public void GrabbedRifle()
     {
         _isEquiped = true;
@@ -113,4 +191,5 @@ public class RifleHandler : MonoBehaviour
     {
         _canFire = false;
     }
+    #endregion
 }
